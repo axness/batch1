2b2eea28a6987e1cab6d13152af6dc50bfa82bde
nageswararao.ullinda <nageswararao.ullinda@sungardas.com>
2018-03-13 12:05:25 +0530

    12 and 13th mar

---
+++ arth.py
@@ -0,0 +1,25 @@
+def addition(firvar, secvar):
+    """
+    it is used to perform addition of two numbers
+
+    **params** **types** **return** **rtype**
+
+    :param firvar: first argument
+    :type firvar: int
+    :praram secvar: second argument
+    :type secvar: int
+    :return: addition of two numbers
+    :rtype: int
+    """
+    return firvar+secvar
+
+def mul(firvar, secvar):
+    return firvar*secvar
+
+def subs(firvar, secvar):
+    return firvar-secvar
+
+def div(a, b):
+    return a/b
+def arth(a, b):
+    return a*b+a+b
---
+++ call_arth.py
@@ -0,0 +1,9 @@
+import arth as a
+#from arth import addition
+from arth import *
+#print a.addition(5, 8)
+#print addition(5, 8)
+#print addition.__doc__
+#print subs.__doc__
+print dir(a)
+print help(a)
